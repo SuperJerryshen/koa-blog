6c20e04ce68510a344139f9742c9706f865fa707
沈经纬 <shenjingwei@shenjingweideMacBook-Pro.local>
2018-03-15 00:34:00 +0800

    更换默认服务器端口

--- server/config.ts
+++ server/config.ts
@@ -1,4 +1,4 @@
-export const port = process.env.PORT || 8080;
+export const port = process.env.PORT || 8086;
 export const bdUrl: string = 'mongodb://localhost/blog';
 export const baseApi: string = 'api';
 export const secret: string = 'this-is-koa-blog-secret';
