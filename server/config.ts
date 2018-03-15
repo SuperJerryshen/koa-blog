51232f60b39dc72c734816f4c78c120922793f9d
沈经纬 <shenjingwei@shenjingweideMacBook-Pro.local>
2018-03-15 14:49:44 +0800

    修改接口前缀

--- server/config.ts
+++ server/config.ts
@@ -1,4 +1,4 @@
 export const port = process.env.PORT || 8086;
 export const bdUrl: string = 'mongodb://localhost/blog';
-export const baseApi: string = 'api';
+export const baseApi: string = 'blog/api';
 export const secret: string = 'this-is-koa-blog-secret';
